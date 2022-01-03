f4629c70674af3542ea5dd987224582fb7103924
ravionl9 <ravi.6sharma@gmail.com>
2022-01-03 16:53:08 +0530

    code uploaded

---
+++ lambda/index.js
@@ -0,0 +1,314 @@
+const Alexa = require('ask-sdk-core');
+const AWS = require('aws-sdk');
+const ddbAdapter = require('ask-sdk-dynamodb-persistence-adapter');
+const Util = require('./util.js');
+
+const LaunchRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Welcome, you can say "play audio" to start listening to music. What would you like to do?';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+const PlayAudioIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && (Alexa.getIntentName(handlerInput.requestEnvelope) === 'PlayAudioIntent'
+                || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.ResumeIntent');
+    },
+    async handle(handlerInput) {
+        const playbackInfo = await getPlaybackInfo(handlerInput);
+
+        const speakOutput = 'Playing the audio stream.';
+        const playBehavior = 'REPLACE_ALL';
+        const podcastUrl = 'https://radiosahaja.com:8443/SYradio.mp3';
+       
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .addAudioPlayerPlayDirective(
+                playBehavior,
+                podcastUrl,
+                playbackInfo.token,
+                playbackInfo.offsetInMilliseconds
+                )
+            .getResponse();
+    }
+};
+
+const PauseAudioIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.PauseIntent';
+    },
+    async handle(handlerInput) {
+        return handlerInput.responseBuilder
+            .addAudioPlayerStopDirective()
+            .getResponse();
+    }
+};
+
+const UnsupportedAudioIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && (
+                Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.LoopOffIntent'
+                    || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.LoopOnIntent'
+                    || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.NextIntent'
+                    || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.PreviousIntent'
+                    || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.RepeatIntent'
+                    || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.ShuffleOffIntent'
+                    || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.ShuffleOnIntent'
+                    || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.StartOverIntent'
+                );
+    },
+    async handle(handlerInput) {
+        const speakOutput = 'Sorry, I can\'t support that yet.';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .getResponse();
+    }
+};
+
+const HelpIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'You can say "play audio" to start playing music! How can I help?';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+const CancelAndStopIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && (Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.CancelIntent'
+                || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.StopIntent');
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Goodbye!';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .getResponse();
+    }
+};
+
+const AudioPlayerEventHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type.startsWith('AudioPlayer.');
+  },
+  async handle(handlerInput) {
+    const playbackInfo = await getPlaybackInfo(handlerInput);
+    
+    const audioPlayerEventName = handlerInput.requestEnvelope.request.type.split('.')[1];
+    console.log(`AudioPlayer event encountered: ${handlerInput.requestEnvelope.request.type}`);
+    let returnResponseFlag = false;
+    switch (audioPlayerEventName) {
+      case 'PlaybackStarted':
+        playbackInfo.token = handlerInput.requestEnvelope.request.token;
+        playbackInfo.inPlaybackSession = true;
+        playbackInfo.hasPreviousPlaybackSession = true;
+        returnResponseFlag = true;
+        break;
+      case 'PlaybackFinished':
+        playbackInfo.inPlaybackSession = false;
+        playbackInfo.hasPreviousPlaybackSession = false;
+        playbackInfo.nextStreamEnqueued = false;
+        returnResponseFlag = true;
+        break;
+      case 'PlaybackStopped':
+        playbackInfo.token = handlerInput.requestEnvelope.request.token;
+        playbackInfo.inPlaybackSession = true;
+        playbackInfo.offsetInMilliseconds = handlerInput.requestEnvelope.request.offsetInMilliseconds;
+        break;
+      case 'PlaybackNearlyFinished':
+        break;
+      case 'PlaybackFailed':
+        playbackInfo.inPlaybackSession = false;
+        console.log('Playback Failed : %j', handlerInput.requestEnvelope.request.error);
+        break;
+      default:
+        break;
+    }
+    setPlaybackInfo(handlerInput, playbackInfo);
+    return handlerInput.responseBuilder.getResponse();
+  },
+};
+
+const PlaybackControllerHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type.startsWith('PlaybackController.');
+  },
+  async handle(handlerInput) {
+    const playbackInfo = await getPlaybackInfo(handlerInput);
+    const playBehavior = 'REPLACE_ALL';
+    const podcastUrl = 'https://radiosahaja.com:8443/SYradio.mp3';
+    const playbackControllerEventName = handlerInput.requestEnvelope.request.type.split('.')[1];
+    let response;
+    switch (playbackControllerEventName) {
+      case 'PlayCommandIssued':
+        response = handlerInput.responseBuilder
+            .addAudioPlayerPlayDirective(
+                playBehavior,
+                podcastUrl,
+                playbackInfo.token,
+                playbackInfo.offsetInMilliseconds
+                )
+            .getResponse();
+        break;
+      case 'PauseCommandIssued':
+        response = handlerInput.responseBuilder
+            .addAudioPlayerStopDirective()
+            .getResponse();
+        break;
+      default:
+        break;
+    }
+    setPlaybackInfo(handlerInput, playbackInfo);
+
+    console.log(`PlayCommandIssued event encountered: ${handlerInput.requestEnvelope.request.type}`);
+    return response;
+  },
+};
+
+const SystemExceptionHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'System.ExceptionEncountered';
+  },
+  handle(handlerInput) {
+    console.log(`System exception encountered: ${JSON.stringify(handlerInput.requestEnvelope.request)}`);
+  },
+};
+
+const FallbackIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.FallbackIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Sorry, I don\'t know about that. Please try again.';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+const SessionEndedRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        console.log(`~~~~ Session ended: ${JSON.stringify(handlerInput.requestEnvelope)}`);
+        
+        return handlerInput.responseBuilder.getResponse();
+    }
+};
+
+const IntentReflectorHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest';
+    },
+    handle(handlerInput) {
+        const intentName = Alexa.getIntentName(handlerInput.requestEnvelope);
+        const speakOutput = `You just triggered ${intentName}`;
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .getResponse();
+    }
+};
+
+const ErrorHandler = {
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        const speakOutput = 'Sorry, I had trouble doing what you asked. Please try again.';
+        console.log(`~~~~ Error handled: ${JSON.stringify(error)}`);
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+/* HELPER FUNCTIONS */
+
+async function getPlaybackInfo(handlerInput) {
+  const attributes = await handlerInput.attributesManager.getPersistentAttributes();
+  return attributes.playbackInfo;
+}
+
+async function setPlaybackInfo(handlerInput, playbackInfoObject) {
+  await handlerInput.attributesManager.setPersistentAttributes({
+      playbackInfo: playbackInfoObject
+      });
+}
+
+const LoadPersistentAttributesRequestInterceptor = {
+  async process(handlerInput) {
+    const persistentAttributes = await handlerInput.attributesManager.getPersistentAttributes();
+
+    if (Object.keys(persistentAttributes).length === 0) {
+      handlerInput.attributesManager.setPersistentAttributes({
+        playbackInfo: {
+          offsetInMilliseconds: 0,
+          token: 'sample-audio-token',
+          inPlaybackSession: false,
+          hasPreviousPlaybackSession: false,
+        },
+      });
+    }
+  },
+};
+
+const SavePersistentAttributesResponseInterceptor = {
+  async process(handlerInput) {
+    await handlerInput.attributesManager.savePersistentAttributes();
+  },
+};
+
+exports.handler = Alexa.SkillBuilders.custom()
+    .addRequestHandlers(
+        LaunchRequestHandler,
+        PlayAudioIntentHandler,
+        PauseAudioIntentHandler,
+        UnsupportedAudioIntentHandler,
+        HelpIntentHandler,
+        CancelAndStopIntentHandler,
+        AudioPlayerEventHandler,
+        PlaybackControllerHandler,
+        SystemExceptionHandler,
+        FallbackIntentHandler,
+        SessionEndedRequestHandler,
+        IntentReflectorHandler)
+    .addErrorHandlers(
+        ErrorHandler)
+    .addRequestInterceptors(LoadPersistentAttributesRequestInterceptor)
+    .addResponseInterceptors(SavePersistentAttributesResponseInterceptor)
+    .withCustomUserAgent('sample/audioplayer-nodejs/v2.0')
+    .withPersistenceAdapter(
+        new ddbAdapter.DynamoDbPersistenceAdapter({
+            tableName: process.env.DYNAMODB_PERSISTENCE_TABLE_NAME,
+            createTable: false,
+            dynamoDBClient: new AWS.DynamoDB({apiVersion: 'latest', region: process.env.DYNAMODB_PERSISTENCE_REGION})
+        })
+    )
+    .lambda();
